b1e28cfd5adbe74ddc7ddf4d02b3fd540e1872a6
ngmicroapp <ngmicroapp@gmail.com>
2021-03-31 20:28:55 +0530

    zoneless-without-directive demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,46 @@
-import { AfterViewChecked, Component, NgZone } from "@angular/core";
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  NgZone,
+  Renderer2,
+  ViewChild
+} from "@angular/core";
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements AfterViewChecked {
+export class AppComponent implements AfterViewInit, AfterViewChecked {
+  @ViewChild("btn") btnEl: ElementRef<HTMLButtonElement>;
 
-  constructor(private readonly zone: NgZone) { }
+  constructor(
+    private readonly zone: NgZone,
+    private readonly renderer: Renderer2
+  ) { }
 
   onClick() {
-    console.log("onClick event...");
+    console.log("onClick");
+  }
+
+  ngAfterViewInit() {
+    this.setupClickListener();
   }
 
   ngAfterViewChecked() {
     console.log("CD performed");
+  }
+
+  private setupClickListener() {
+    this.zone.runOutsideAngular(() => {
+      this.setupClickListenerViaRenderer();
+    });
+  }
+  private setupClickListenerViaRenderer() {
+    this.renderer.listen(this.btnEl.nativeElement, "click", () => {
+      console.log("onClick");
+    });
   }
 }
