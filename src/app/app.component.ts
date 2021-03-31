87f7a08b867f18a4314ac8243947f666812df239
ngmicroapp <ngmicroapp@gmail.com>
2021-03-31 20:23:53 +0530

    zonless-demo-with-directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, NgZone } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'change-detection';
+export class AppComponent implements AfterViewChecked {
+
+  constructor(private readonly zone: NgZone) { }
+
+  onClick() {
+    console.log("onClick event...");
+  }
+
+  ngAfterViewChecked() {
+    console.log("CD performed");
+  }
 }
---
+++ src/app/click-zoneless.directive.ts
@@ -0,0 +1,46 @@
+import {
+  Directive,
+  ElementRef,
+  EventEmitter,
+  NgZone,
+  OnDestroy,
+  OnInit,
+  Output,
+  Renderer2
+} from "@angular/core";
+@Directive({
+  selector: "[appZoneless]"
+})
+export class ClickZonelessDirective implements OnInit, OnDestroy {
+  @Output("appZoneless") clickZoneless = new EventEmitter<MouseEvent>();
+
+  private teardownLogicFn: Function;
+
+  constructor(
+    private readonly zone: NgZone,
+    private readonly el: ElementRef,
+    private readonly renderer: Renderer2
+  ) { }
+
+  ngOnInit() {
+    this.zone.runOutsideAngular(() => {
+      this.setupClickListener();
+    });
+  }
+
+  ngOnDestroy() {
+    this.teardownLogicFn();
+  }
+
+  private setupClickListener() {
+    this.teardownLogicFn = this.renderer.listen(
+      this.el.nativeElement,
+      "click",
+      (event: MouseEvent) => {
+        this.clickZoneless.emit(event);
+      }
+    );
+  }
+}
+
+// console output: onClick
